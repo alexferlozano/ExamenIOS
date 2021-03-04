124474817c1a8545fe86021966f3149469d78f0b
GabrielRuiseco <19170012@uttcampus.edu.mx>
2021-03-04 10:09:49 -0600

    Initial Commit

---
+++ AlexExamen/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AlexExamen
+//
+//  Created by mac13 on 04/03/21.
+//  Copyright © 2021 UTT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
